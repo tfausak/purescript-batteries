13e2d89dca12a1d6fda976203c68cc8c0808750c
Taylor Fausak <taylor@fausak.me>
2015-10-21 12:37:43 -0500

    Add purescript-console

--- src/Batteries.purs
+++ src/Batteries.purs
@@ -2,9 +2,11 @@ module Batteries
   ( module Batteries
   , module Control.Monad.Eff
   , module Control.Monad.Eff.Class
+  , module Control.Monad.Eff.Console
   , module Prelude
   ) where
 
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
+import Control.Monad.Eff.Console
 import Prelude
