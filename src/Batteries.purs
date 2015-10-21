5bf0cf649268965c5667323aef8954b6a05bf4f7
Taylor Fausak <taylor@fausak.me>
2015-10-21 12:48:24 -0500

    Add purescript-control

--- src/Batteries.purs
+++ src/Batteries.purs
@@ -1,12 +1,34 @@
 module Batteries
   ( module Batteries
+  , module Control.Alt
+  , module Control.Alternative
+  , module Control.Apply
+  , module Control.Bind
+  , module Control.Comonad
+  , module Control.Extend
+  , module Control.Lazy
+  , module Control.Monad
   , module Control.Monad.Eff
   , module Control.Monad.Eff.Class
   , module Control.Monad.Eff.Console
+  , module Control.MonadPlus
+  , module Control.Plus
+  , module Data.Functor
   , module Prelude
   ) where
 
+import Control.Alt
+import Control.Alternative
+import Control.Apply
+import Control.Bind
+import Control.Comonad
+import Control.Extend
+import Control.Lazy
+import Control.Monad
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
 import Control.Monad.Eff.Console
+import Control.MonadPlus
+import Control.Plus
+import Data.Functor
 import Prelude
