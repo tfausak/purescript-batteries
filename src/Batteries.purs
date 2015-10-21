024b8e33e51a91942c07d5401ad90a479f22b136
Taylor Fausak <taylor@fausak.me>
2015-10-21 12:35:36 -0500

    Add purescript-eff

--- src/Batteries.purs
+++ src/Batteries.purs
@@ -1,6 +1,10 @@
 module Batteries
   ( module Batteries
+  , module Control.Monad.Eff
+  , module Control.Monad.Eff.Class
   , module Prelude
   ) where
 
+import Control.Monad.Eff
+import Control.Monad.Eff.Class
 import Prelude
